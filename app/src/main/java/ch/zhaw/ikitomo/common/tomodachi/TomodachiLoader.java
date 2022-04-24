4da33e576c742ecfeac8dace320f40522f11e33b
zumbrseb <sebastian.zumbrunn@students.zhaw.ch>
2022-04-24 20:38:53 +0200

    add methods to TomodachiLoader
    Closes #1

--- app/src/main/java/ch/zhaw/ikitomo/common/tomodachi/TomodachiLoader.java
+++ app/src/main/java/ch/zhaw/ikitomo/common/tomodachi/TomodachiLoader.java
@@ -1,5 +1,37 @@
 package ch.zhaw.ikitomo.common.tomodachi;
 
-public class TomodachiLoader {
+import java.io.IOException;
+import java.nio.file.Path;
 
+/**
+ * A helper class to load {@link TomodachiFile} objects
+ */
+public class TomodachiLoader {
+    /**
+     * private constructor
+     */
+    private TomodachiLoader() {
+    }
+
+    /**
+     * Loads a {@link TomodachiFile} from the given root folder
+     * 
+     * @param path the path to the root folder of the tomodachi
+     * @return the loaded Tomodachi File
+     * @throws IOException if the file could not be loaded
+     */
+    public static TomodachiFile load(String rootFolder) throws IOException {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    /**
+     * Saves the given {@link TomodachiFile} to the root folder set in the
+     * {@link TomodachiFile#getRootFolder()}
+     * 
+     * @param file the tomodachi file to save
+     */
+    public static void save(TomodachiFile file) {
+        Path tomodachiFile = file.getRootFolder().resolve("tomodachi.json");
+        throw new UnsupportedOperationException("not implemented yet");
+    }
 }
