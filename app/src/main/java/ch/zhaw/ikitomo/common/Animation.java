c9cbcd8b63fb4a8730dcb6587ec7b7562068845e
zumbrseb <zumbrseb@students.zhaw.ch>
2022-04-25 13:59:02 +0200

    Update app/src/main/java/ch/zhaw/ikitomo/common/Animation.java
    Co-authored-by: Jonas Costa <costajon@students.zhaw.ch>

--- app/src/main/java/ch/zhaw/ikitomo/common/Animation.java
+++ app/src/main/java/ch/zhaw/ikitomo/common/Animation.java
@@ -5,8 +5,8 @@ import javafx.scene.image.Image;
 /**
  * Represents an animation
  * 
- * @param duration the duration of each frame
- * @param image    the image of the animation
+ * @param duration The duration of each frame
+ * @param image    The image of the animation
  */
 public record Animation(int duration, Image image) {
 
