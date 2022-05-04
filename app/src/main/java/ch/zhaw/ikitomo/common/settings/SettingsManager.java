c596e3883d542e52f38af2fea400cff8f429ed48
Manuel Thalmann <m@nuth.ch>
2022-05-04 13:13:23 +0200

    Remove unnecessary methods

--- app/src/main/java/ch/zhaw/ikitomo/common/settings/SettingsManager.java
+++ app/src/main/java/ch/zhaw/ikitomo/common/settings/SettingsManager.java
@@ -19,26 +19,4 @@ public class SettingsManager extends JSONManager<Settings> {
     public SettingsManager() {
         super(Settings.class);
     }
-
-    /**
-     * Loads the settings from the given file.
-     * 
-     * @param file The file to load the settings from
-     * @return The loaded settings
-     * @throws IOException Occurs when the file could not be read
-     */
-    public Settings load(String path) throws IOException {
-        return super.load(path);
-    }
-
-    /**
-     * Saves the settings to the given file.
-     * 
-     * @param settings The settings to save
-     * @param path     The name of the file to save the settings to
-     * @throws IOException Occurs when the file could not be written
-     */
-    public void save(String path, Settings settings) throws IOException {
-        super.save(path, settings);
-    }
 }
