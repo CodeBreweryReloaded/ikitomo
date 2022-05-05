5b87a42d09c0fb1573728314efe9489254f27396
zumbrseb <sebastian.zumbrunn@students.zhaw.ch>
2022-05-05 15:26:11 +0200

    Add bindings to the current selected tomodachi

--- app/src/main/java/ch/zhaw/ikitomo/common/tomodachi/TomodachiEnvironment.java
+++ app/src/main/java/ch/zhaw/ikitomo/common/tomodachi/TomodachiEnvironment.java
@@ -1,16 +1,40 @@
 package ch.zhaw.ikitomo.common.tomodachi;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
+import ch.zhaw.ikitomo.common.JavaFXUtils;
 import ch.zhaw.ikitomo.common.settings.Settings;
 import ch.zhaw.ikitomo.common.settings.SettingsManager;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.ObjectBinding;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 
 /**
  * Represents the environment of the tomodachi application
  */
 public class TomodachiEnvironment {
+    /**
+     * The path to the folder where by default all tomodachi folders are stored
+     */
+    private static final String DEFAULT_TOMODACHI_FOLDER_PATH = "./tomodachi";
+
+    /**
+     * The id of the default tomodachi
+     */
+    private static final String DEFAULT_TOMODACHI_ID = "ch.zhaw.ikitomo.neko";
+
     /**
      * A component for logging messages.
      */
@@ -32,9 +56,34 @@ public class TomodachiEnvironment {
     private Settings settings = null;
 
     /**
+     * All available tomodachis
+     */
+    private ObservableMap<String, TomodachiDefinition> tomodachiDefinitionMap = FXCollections.observableHashMap();
+
+    /**
+     * An observable list of all tomodachi definitions. It is dependent on
+     * {@link #tomodachiDefinitionMap}
+     */
+    private ObservableList<TomodachiDefinition> tomodachiDefinitionList = JavaFXUtils
+            .observableValuesFromMap(tomodachiDefinitionMap);
+
+    /**
+     * A binding of the currently selected tomodachi definition in the
+     * {@link #settings} object
+     */
+    private ObjectBinding<TomodachiDefinition> currentTomodachiDefinition = Bindings.createObjectBinding(
+            this::getCurrentTomodachiDefinitionFromTomodachiDefinitionMap, settings.tomodachiIDProperty());
+
+    /**
+     * The default tomodachi definition. This is loaded in
+     * {@link #reloadTomodachis()} and should always be a non-null value
+     */
+    private TomodachiDefinition defaultTomodachiDefinition = null;
+
+    /**
      * Initializes a new instance of the {@link TomodachiEnvironment} class
      *
-     * @param settingsManager
+     * @param settingsManager the settings manager
      */
     public TomodachiEnvironment(SettingsManager settingsManager, TomodachiManager tomodachiManager) {
         this.settingsManager = settingsManager;
@@ -69,20 +118,129 @@ public class TomodachiEnvironment {
     }
 
     /**
-     * Loads the settings from the default location
-     *
-     * @return The settings of the application
-     * @throws IOException Occurs when the settings could not be loaded
+     * Loads the settings from the default location and loads all available
+     * tomodachis
      */
-    public Settings load() {
+    public void load() {
         try {
             settings = settingsManager.load(SettingsManager.DEFAULT_SETTINGS_PATH);
         } catch (IOException e) {
             LOGGER.log(Level.WARNING, "The settings could not be loaded.", e);
             settings = new Settings();
         }
+        reloadTomodachis().join();
+    }
 
-        return settings;
+    /**
+     * Reloads all available tomodachis in a separate thread. The method returns a
+     * {@link CompletableFuture} which completes when the reloading is finished. The
+     * {@link #tomodachiDefinitionMap} list is updated
+     * 
+     * @return A future which completes when the operation is finished
+     */
+    public CompletableFuture<Void> reloadTomodachis() {
+        return CompletableFuture.supplyAsync(this::readAvailableTomodachis)
+                .thenAccept(this::updateTomodachiDefinitions);
+    }
+
+    /**
+     * Reloads all available tomodachis
+     */
+    private List<TomodachiDefinition> readAvailableTomodachis() {
+        List<TomodachiDefinition> localTomodachiDefinitions = new ArrayList<>();
+        File tomodachiRootFolder = new File(DEFAULT_TOMODACHI_FOLDER_PATH);
+        File[] tomodachiFolders = tomodachiRootFolder.listFiles();
+        if (tomodachiFolders == null) {
+            LOGGER.warning("The tomodachi folder \"" + tomodachiRootFolder.getAbsolutePath() + "\"could not be found.");
+            return Collections.emptyList();
+        }
+        for (File tomodachiFolder : tomodachiFolders) {
+            try {
+                localTomodachiDefinitions.add(tomodachiManager.load(tomodachiFolder.getAbsolutePath()));
+            } catch (IOException e) {
+                LOGGER.log(Level.WARNING,
+                        "The tomodachi definition \"" + tomodachiFolder.getAbsolutePath() + "\" could not be loaded.",
+                        e);
+            }
+        }
+        return localTomodachiDefinitions;
+    }
+
+    /**
+     * Updates the {@link #tomodachiDefinitionMap} list in the JavaFX thread. Coll
+     * 
+     * @param list The new list
+     */
+    private CompletableFuture<Void> updateTomodachiDefinitions(List<TomodachiDefinition> list) {
+        Map<String, TomodachiDefinition> map = list.stream()
+                .collect(Collectors.toMap(TomodachiDefinition::getID, Function.identity()));
+        TomodachiDefinition defaultTomodachi = map.get(DEFAULT_TOMODACHI_ID);
+        if (defaultTomodachi == null) {
+            // TODO: add default tomodachi to resource and load it
+            throw new IllegalStateException("The default tomodachi \"" + DEFAULT_TOMODACHI_ID + "\" doesn't exist");
+        }
+        currentTomodachiDefinitionBinding().invalidate(); // invalidate binging in case the default tomodachi changed
+        return JavaFXUtils.runLater(() -> {
+            tomodachiDefinitionMap.clear();
+            tomodachiDefinitionMap.putAll(map);
+            this.defaultTomodachiDefinition = defaultTomodachi;
+        });
+    }
+
+    /**
+     * Gets all available tomodachi definitions
+     * 
+     * @return The list of all available tomodachi definitions
+     */
+    public ObservableList<TomodachiDefinition> getTomodachiDefinitions() {
+        return tomodachiDefinitionList;
+    }
+
+    /**
+     * Gets the tomodachi definition with the given id or null if the id doesn't
+     * exist
+     * 
+     * @param id The id to look up
+     * @return The tomodachi definition with the given id or null if the id doesn't
+     *         exist
+     */
+    public TomodachiDefinition getTomodachiDefinition(String id) {
+        return tomodachiDefinitionMap.get(id);
+    }
+
+    /**
+     * Gets the tomodachi definition currently selected in
+     * {@link #settings#getCurrentTomodachiDefinition()}.
+     * If the currently selected tomodachi id doesn't exist then the
+     * {@link #defaultTomodachiDefinition} is returned
+     * 
+     * @return The tomodachi definition
+     */
+    public TomodachiDefinition getCurrentTomodachiDefinition() {
+        return currentTomodachiDefinition.get();
+    }
+
+    /**
+     * An internal helper method with gets the tomodachi definition currently
+     * selected in the {@link #settings} object.
+     * If the currently selected tomodachi id doesn't exist then the
+     * {@link #defaultTomodachiDefinition} is returned
+     * 
+     * @return The tomodachi definition or {@link #defaultTomodachiDefinition} if
+     *         the currently selected tomodachi id doesn't exist
+     */
+    private TomodachiDefinition getCurrentTomodachiDefinitionFromTomodachiDefinitionMap() {
+        return tomodachiDefinitionMap.getOrDefault(settings.getTomodachiID(), defaultTomodachiDefinition);
+    }
+
+    /**
+     * Gets a {@link ObjectBinding} to the currently selected tomodachi definition
+     * 
+     * @return The binding
+     * @see #getCurrentTomodachiDefinition()
+     */
+    public ObjectBinding<TomodachiDefinition> currentTomodachiDefinitionBinding() {
+        return currentTomodachiDefinition;
     }
 
     /**
@@ -93,4 +251,5 @@ public class TomodachiEnvironment {
     public void save() throws IOException {
         settingsManager.save(SettingsManager.DEFAULT_SETTINGS_PATH, settings);
     }
+
 }
